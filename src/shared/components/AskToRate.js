ee5a497547dd32a576ccce8accd24008f3646884
Behnam Azimi <bhnmzm@gmail.com>
2023-03-25 09:50:27 +0100

    Update AskToRate content

--- src/shared/components/AskToRate.js
+++ src/shared/components/AskToRate.js
@@ -1,6 +1,7 @@
 import {REVIEW_US} from "../utils/constants";
 import cx from "classnames";
 import {useData} from "../../popup/context/data.context";
+import {FiStar} from "react-icons/fi";
 
 const AskToRate = () => {
   const {reviewLinksClicksCount, countUpReviewLinksClicks} = useData()
@@ -9,10 +10,31 @@ const AskToRate = () => {
   const visible = reviewLinksClicksCount < 2
   if (!visible) return null
 
+  let reviewText = "Give us stars"
   const randomText = REVIEW_US[Math.floor(Math.random() * REVIEW_US.length)];
+
+  if (Math.random() > 0.9) {
+    reviewText = randomText
+  }
 
   const handleOnClick = () => countUpReviewLinksClicks()
 
+  return (
+    <div className="AskToRate">
+      {reviewText}
+
+      <a href="https://bit.ly/rate-mwd" target="_blank"
+         onClick={handleOnClick}>
+          <span>
+            <FiStar/>
+            <FiStar/>
+            <FiStar/>
+            <FiStar/>
+            <FiStar/>
+        </span>
+      </a>
+    </div>
+  )
   return (
     <div className={cx("AskToRate", visible && "visible")}>
       <a href="https://bit.ly/rate-mwd" target="_blank"
